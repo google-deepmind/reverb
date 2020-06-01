4b189b724a98ac48cb5b89df736d44fd286ad5a5
Gabriel Barth-Maron <gabrielbm@google.com>
2020-06-01 08:00:06 -0700

    Remove tag for Python 3.5 since Reverb does not support it.
    Reverb makes use of f-strings, which were introduced in 3.6.
    
    PiperOrigin-RevId: 314130033
    Change-Id: I6803dcf42aadb3a6305ea8ce39457ef40d82fdd5

--- reverb/pip_package/setup.py
+++ reverb/pip_package/setup.py
@@ -95,7 +95,6 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
