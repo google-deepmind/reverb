89eb405f5a9efa27f75055c27df2c8ca59c83f7b
Albin Cassirer <cassirer@google.com>
2020-06-14 07:23:06 -0700

    Reverb: Add a Cleanup, a simple class that executes a callback when destroyed.
    PiperOrigin-RevId: 316339152
    Change-Id: Ie2c8c613f460dcb7bc7c0febe293768bc0f4d9a8

---
+++ reverb/cc/support/cleanup.h
@@ -0,0 +1,51 @@
+// Copyright 2019 DeepMind Technologies Limited.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef REVERB_CC_SUPPORT_CLEANUP_H_
+#define REVERB_CC_SUPPORT_CLEANUP_H_
+
+#include "absl/base/attributes.h"
+#include "absl/memory/memory.h"
+
+namespace deepmind {
+namespace reverb {
+namespace internal {
+
+// Calls provided callback when object destroyed.
+template <typename Callback>
+class Cleanup {
+ public:
+  explicit Cleanup(Callback&& callback)
+      : callback_(std::forward<Callback>(callback)) {}
+  ~Cleanup() { callback_(); }
+
+  // Cleanup is neither copyable nor movable.
+  Cleanup(const Cleanup&) = delete;
+  Cleanup& operator=(const Cleanup&) = delete;
+
+ private:
+  Callback callback_;
+};
+
+template <typename Callback>
+ABSL_MUST_USE_RESULT std::unique_ptr<Cleanup<Callback>> MakeCleanup(
+    Callback&& callback) {
+  return absl::make_unique<Cleanup<Callback>>(std::forward<Callback>(callback));
+}
+
+}  // namespace internal
+}  // namespace reverb
+}  // namespace deepmind
+
+#endif  // REVERB_CC_SUPPORT_CLEANUP_H_
