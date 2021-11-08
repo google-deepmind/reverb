google-deepmind/reverb

commit 8e194fb91ee93640e7ea25c3661585d182be283c

[Reverb] More informative error messages for undefined symbol errors in OSS.
Before:
ImportError: /home/ebrevdo/.local/lib/python3.9/site-packages/reverb/libpybind.so: undefined symbol: _ZN4absl12lts_202103246Status16kMovedFromStringE

After:

ImportError:
Attempted to load a reverb dynamic library, but it could not find the required
symbols inside of TensorFlow.  This commonly occurs when your version of
tensorflow and reverb are mismatched.  For example, if you are using the python
package 'tf-nightly', make sure you use the python package 'dm-reverb-nightly'
built on the same or the next night.  If you are using a release version package
'tensorflow', use a release package 'dm-reverb' built to be compatible with
that exact version.  If all else fails, file a github issue on deepmind/reverb.
Current installed version of tensorflow: 2.6.0.

Orignal error:
/home/ebrevdo/.local/lib/python3.9/site-packages/reverb/libpybind.so: undefined symbol: _ZN4absl12lts_202103246Status16kMovedFromStringE

PiperOrigin-RevId: 405440707
Change-Id: Id178e64a115b98a17bd4d528035ce63e0abf064d

reverb/cc/platform/default/build_rules.bzl
@@ -319,20 +319,26 @@ def reverb_gen_op_wrapper_py(name, out, kernel_lib, linkopts = [], **kwargs):
     native.genrule(
         name = "{}_genrule".format(out),
         outs = [out],
-        cmd = """
-        echo 'import tensorflow as tf
-_reverb_gen_op = tf.load_op_library(
-    tf.compat.v1.resource_loader.get_path_to_datafile(
-       "lib{}_gen_op.so"))
+        cmd = """echo 'import tensorflow as _tf
+from reverb.platform.default import load_op_library as _load_op_library
+
+try:
+  _reverb_gen_op = _tf.load_op_library(
+    _tf.compat.v1.resource_loader.get_path_to_datafile("lib{}_gen_op.so"))
+except tf.errors.NotFoundError as e:
+  _load_op_library.reraise_wrapped_error(e)
 _locals = locals()
 for k in dir(_reverb_gen_op):
   _locals[k] = getattr(_reverb_gen_op, k)
 del _locals' > $@""".format(name),
     )
+    deps = kwargs.pop("deps", [])
+    deps.append("//reverb/platform/default:load_op_library")
     native.py_library(
         name = name,
         srcs = [out],
         data = [":lib{}_gen_op.so".format(name)],
+        deps = deps,
         **kwargs
     )
 
@@ -478,10 +484,13 @@ def reverb_pybind_extension(
     native.genrule(
         name = name + "_py_file",
         outs = [py_file],
-        cmd = (
-            "echo 'import tensorflow as _tf; from .%s import *; del _tf' >$@" %
-            module_name
-        ),
+        cmd = """echo 'import tensorflow as _tf
+from reverb.platform.default import load_op_library as _load_op_library
+try:
+  from .%s import *
+except ImportError as e:
+  _load_op_library.reraise_wrapped_error(e)
+del _tf' >$@""" % module_name,
         output_licenses = ["unencumbered"],
         visibility = visibility,
         testonly = testonly,
@@ -489,6 +498,7 @@ def reverb_pybind_extension(
     native.py_library(
         name = name,
         data = [so_file],
+        deps = ["//reverb/platform/default:load_op_library"],
         srcs = [py_file],
         srcs_version = srcs_version,
         licenses = licenses,

reverb/platform/default/BUILD
@@ -8,6 +8,12 @@ package(default_visibility = ["//reverb:__subpackages__"])
 licenses(["notice"])
 
 reverb_pytype_strict_library(
+    name = "load_op_library",
+    srcs = ["load_op_library.py"],
+    srcs_version = "PY3",
+)
+
+reverb_pytype_strict_library(
     name = "checkpointers",
     srcs = ["checkpointers.py"],
     srcs_version = "PY3",

reverb/platform/default/load_op_library.py
@@ -1,0 +1,47 @@
+# Copyright 2019 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helpers for loading dynamic library files."""
+import sys
+
+import six
+import tensorflow as tf
+
+UNDEFINED_SYMBOL_ERROR_MESSAGE = """
+Attempted to load a reverb dynamic library, but it could not find the required
+symbols inside of TensorFlow.  This commonly occurs when your version of
+tensorflow and reverb are mismatched.  For example, if you are using the python
+package 'tf-nightly', make sure you use the python package 'dm-reverb-nightly'
+built on the same or the next night.  If you are using a release version package
+'tensorflow', use a release package 'dm-reverb' built to be compatible with
+that exact version.  If all else fails, file a github issue on deepmind/reverb.
+Current installed version of tensorflow: {tf_version}.
+""".format(tf_version=tf.__version__)
+
+
+def reraise_wrapped_error(error: Exception):
+  """Wraps failures with better error messages.
+
+  Args:
+    error: The exception.  We must be inside a raise.
+
+  Raises:
+    ImportError: Typically if there is a version mismatch.
+  """
+  if 'undefined symbol' in str(error).lower():
+    six.reraise(ImportError,
+                ImportError('%s\nOrignal error:\n%s' % (
+                    UNDEFINED_SYMBOL_ERROR_MESSAGE, error)),
+                sys.exc_info()[2])
+  raise error

commit 2e5086b43972896b32394185cf26e8cc4295bf40

Fix typo in gen_reverb_ops.py (for import error messages)
The intended error message introduced in 8e194fb (cl/405440707) actually
does not work in OSS, because of a typo. This commit fixes the typo.

reverb/cc/platform/default/build_rules.bzl
@@ -325,7 +325,7 @@ from reverb.platform.default import load_op_library as _load_op_library
 try:
   _reverb_gen_op = _tf.load_op_library(
     _tf.compat.v1.resource_loader.get_path_to_datafile("lib{}_gen_op.so"))
-except tf.errors.NotFoundError as e:
+except _tf.errors.NotFoundError as e:
   _load_op_library.reraise_wrapped_error(e)
 _locals = locals()
 for k in dir(_reverb_gen_op):