8b8aaa3ef9815f357e7bde5543e2699a187b19c2
Albin Cassirer <cassirer@google.com>
2022-01-21 00:51:57 -0800

    Remove deprecated TFClient.dataset.
    PiperOrigin-RevId: 423257197
    Change-Id: I5210416adef49facb0a1d07f134c2906719ec5b0

--- reverb/tf_client.py
+++ reverb/tf_client.py
@@ -15,18 +15,13 @@
 
 """TFClient provides tf-ops for interacting with Reverb."""
 
-from typing import Any, Optional, Sequence
+from typing import Optional, Sequence
 
-from absl import logging
-from reverb import dataset
 from reverb import replay_sample
 import tensorflow.compat.v1 as tf
 import tree
 
 from reverb.cc.ops import gen_reverb_ops
-
-# TODO(b/153616873): Remove this alias once users have been refactored.
-ReplayDataset = dataset.ReplayDataset
 
 
 class TFClient:
@@ -140,78 +135,3 @@ class TFClient:
                        ['update_priorities']) as scope:
       return gen_reverb_ops.reverb_client_update_priorities(
           self._handle, table, keys, priorities, name=scope)
-
-  def dataset(self,
-              table: str,
-              dtypes: Sequence[Any],
-              shapes: Sequence[Any],
-              capacity: int = 100,
-              num_workers_per_iterator: int = -1,
-              max_samples_per_stream: int = -1,
-              sequence_length: Optional[int] = None,
-              emit_timesteps: bool = True,
-              rate_limiter_timeout_ms: int = -1) -> dataset.ReplayDataset:
-    """DEPRECATED, please use dataset.ReplayDataset instead.
-
-    A tf.data.Dataset which samples timesteps from the ReverbService.
-
-    Note: Uses of Python lists are converted into tuples as nest used by the
-    tf.data API doesn't have good support for lists.
-
-    See dataset.ReplayDataset for detailed documentation.
-
-    Args:
-      table: Probability table to sample from.
-      dtypes: Dtypes of the data output. Can be nested.
-      shapes: Shapes of the data output. Can be nested. When `emit_timesteps` is
-        True this is the shape of a single timestep in the sampled items; when
-        it is False shapes must include `sequence_length`.
-      capacity: (Defaults to 100) Maximum number of samples requested by the
-        workers with each request. Higher values give higher throughput but too
-        big values can result in skewed sampling distributions as large number
-        of samples are fetched from single snapshot of the replay (followed by a
-        period of lower activity as the samples are consumed). A good rule of
-        thumb is to set this value to 2-3x times the batch size used.
-      num_workers_per_iterator: (Defaults to -1, i.e auto selected) The number
-        of worker threads to create per dataset iterator. When the selected
-        table uses a FIFO sampler (i.e a queue) then exactly 1 worker must be
-        used to avoid races causing invalid ordering of items. For all other
-        samplers, this value should be roughly equal to the number of threads
-        available on the CPU.
-      max_samples_per_stream: (Defaults to -1, i.e auto selected) The maximum
-        number of samples to fetch from a stream before a new call is made.
-        Keeping this number low ensures that the data is fetched uniformly from
-        all server.
-      sequence_length: (Defaults to None, i.e unknown) The number of timesteps
-        that each sample consists of. If set then the length of samples received
-        from the server will be validated against this number.
-      emit_timesteps: (Defaults to True) If set, timesteps instead of full
-        sequences are retturned from the dataset. Returning sequences instead of
-        timesteps can be more efficient as the memcopies caused by the splitting
-        and batching of tensor can be avoided. Note that if set to False then
-        then all `shapes` must have dim[0] equal to `sequence_length`.
-      rate_limiter_timeout_ms: (Defaults to -1: infinite).  Timeout (in
-        milliseconds) to wait on the rate limiter when sampling from the table.
-        If `rate_limiter_timeout_ms >= 0`, this is the timeout passed to
-        `Table::Sample` describing how long to wait for the rate limiter to
-        allow sampling. The first time that a request times out (across any of
-        the workers), the Dataset iterator is closed and the sequence is
-        considered finished.
-
-    Returns:
-      A ReplayDataset with the above specification.
-    """
-    logging.warning(
-        'TFClient.dataset is DEPRECATED! Please use ReplayDataset (see '
-        './dataset.py) instead.')
-    return dataset.ReplayDataset(
-        server_address=self._server_address,
-        table=table,
-        dtypes=dtypes,
-        shapes=shapes,
-        max_in_flight_samples_per_worker=capacity,
-        num_workers_per_iterator=num_workers_per_iterator,
-        max_samples_per_stream=max_samples_per_stream,
-        sequence_length=sequence_length,
-        emit_timesteps=emit_timesteps,
-        rate_limiter_timeout_ms=rate_limiter_timeout_ms)
