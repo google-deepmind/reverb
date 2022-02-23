71ecfbc44005249c95cb75c95b64df9aa6413ab9
Albin Cassirer <cassirer@google.com>
2022-02-23 01:09:36 -0800

    Internal change.
    PiperOrigin-RevId: 430392532
    Change-Id: Ia8e860c459fa341115e63a9c077611e1707d8543

--- reverb/cc/table_extensions/insert_on_sample.h
+++ reverb/cc/table_extensions/insert_on_sample.h
@@ -31,8 +31,8 @@ class InsertOnSampleExtension : public TableExtensionBase {
   // `target_table`, throwing away the item to insert. absl::InfiniteDuration()
   // will block on the successful insertion.
   // If `target_table` can block on inserts, prefer using a short timeout.
-  explicit InsertOnSampleExtension(std::shared_ptr<Table> target_table,
-                                   absl::Duration timeout);
+  InsertOnSampleExtension(std::shared_ptr<Table> target_table,
+                          absl::Duration timeout);
 
   // Inserts a copy of the item into the target table.
   void ApplyOnSample(const ExtensionItem& item) override;
